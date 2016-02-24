2ab94c714e4a248f3b455144ca07e53eef5d8a7d
Mohammad Rezaei <mhmmdrzaei@gmail.com>
2016-02-24 10:26:11 -0500

    added psudo code to js

---
+++ style.js
@@ -0,0 +1,11 @@
+//type in city 
+// on click spit back results  that then goes to google map, and also appends on page
+
+
+//take a string run it through database, throws back result gives you lat long
+//we take the lat long, use google maps api, make points in the map. 
+
+//using brewery api, grab the information needed from array ie logo, phone number hours, organic etc
+
+
+// add jquery styling for drop down on click drop down 
